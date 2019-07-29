b606766b4044a08036e3da3cbdcb98253663b4fe
An Vo <akira.an111@gmail.com>
2019-07-29 11:44:20 +0700

    add some simple model test and config simplecov gem

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 255}
+  
+  
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
+  context "create new todo" do
+    it "should not accept an record with nil title" do
+      todo = Todo.new(content: 'somthing')
+      expect(todo.valid?).to be_falsy
+      
+    end
 
+    it "should not accept an record with empty title" do
+      todo = Todo.new(title: ' ', content: 'somthing')
+      expect(todo.valid?).to be_falsy
+    end
+
+    it "should not accept an record with too long title" do
+      title = 't'*51;
+      todo = Todo.new(title: title, content: 'somthing')
+      expect(todo.valid?).to be_falsy
+    end
+
+    it "should not accept an record with empty content" do
+      todo = Todo.new(title: 'a', content: '')
+      expect(todo.valid?).to be_falsy
+    end
+    
+    it "should not accept an record with too long content" do
+      content = 'c'*256;
+      todo = Todo.new(title: 'a', content: content)
+      expect(todo.valid?).to be_falsy
+    end
+  end
   
 end
